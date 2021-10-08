05061ebba4cc5498c00c4194f578f397a25ae741
TorryDo <neemon.teen@gmail.com>
2021-10-08 11:08:49 +0700

    update

--- vocabSource/src/main/java/com/torrydo/vocabsource/eng/models/InnerEngResult.kt
+++
@@ -1,7 +0,0 @@
-package com.torrydo.vocabsource.eng.models
-
-data class InnerEngResult (
-    val title : String,
-    val examples : String,
-    var isExpanded : Boolean
-)
--- vocabSource/src/main/java/com/torrydo/vocabsource/vi/ViResult.kt
+++
@@ -1,5 +0,0 @@
-package com.torrydo.vocabsource.vi
-
-data class ViResult(
-    val title: String
-)
