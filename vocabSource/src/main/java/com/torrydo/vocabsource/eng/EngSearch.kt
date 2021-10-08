91ae24d2908084c188adebacadb208887eb7c790
TorryDo <neemon.teen@gmail.com>
2021-10-08 10:43:26 +0700

    add app module

--- vocabSource/src/main/java/com/torrydo/vocabsource/eng/EngSearch.kt
+++
@@ -1,9 +0,0 @@
-package com.torrydo.vocabsource.eng
-
-import com.torrydo.vocabsource.ResponseVocabList
-
-interface EngSearch {
-
-    fun getResult(keyWord: String, responseVocabList: ResponseVocabList)
-
-}
